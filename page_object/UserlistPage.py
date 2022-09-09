1e8ecc0665131c44420d66e831d561f2fc017f05
Zxd202 <784433201@qq.com>
2022-09-09 22:11:26 +0800

    用户列表元素定位

--- page_object/UserlistPage.py
+++ page_object/UserlistPage.py
@@ -3,3 +3,22 @@
 from selenium.webdriver.common.by import By
 from utils.Driverunit import Driver
 from time import sleep
+
+
+class User_Page_Locator(object):
+    def __init__(self):
+        self.driver = Driver.get_driver()
+        #添加用户按钮元素定位
+        self.add_user_button = (By.XPATH,"//span[text()='添加用户']")
+        #用户名输入框元素定位
+        self.new_user_input = (By.XPATH,"//*[@id='app']/section/section/main/div/div[3]/div/div[2]/form/div[1]/div/div[1]/input")
+        #密码输入框元素定位
+        self.password_input = (By.XPATH,"//input[@type='password']")
+        #邮箱输入框元素定位
+        self.email_input = (By.XPATH,"//*[@id='app']/section/section/main/div/div[3]/div/div[2]/form/div[3]/div/div[1]/input")
+        #状态选项元素定位
+        self.status_option = (By.XPATH,"//*[@id='app']/section/section/main/div/div[3]/div/div[2]/form/div[4]/div/div/div/input")
+        #正常选项元素定位
+        self.normal_option = (By.XPATH,"//span[text()='正常']")
+        #禁用选项元素定位
+        self.forbidden_option = (By.XPATH, "//span[text()='禁用']")
